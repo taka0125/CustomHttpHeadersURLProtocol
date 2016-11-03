54defbe805dc085ae4732ea78ac5b820d0072675
Alexandre Mantovani Tavares <alexandre@live.in>
2016-11-03 17:37:38 -0200

    Create shared framework target to work with Carthage

---
+++ Example/CustomHttpHeadersURLProtocol/CustomHttpHeadersURLProtocol.h
@@ -0,0 +1,19 @@
+//
+//  CustomHttpHeadersURLProtocol.h
+//  CustomHttpHeadersURLProtocol
+//
+//  Created by Alexandre Tavares on 03/11/16.
+//  Copyright © 2016 Takahiro Ooishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomHttpHeadersURLProtocol.
+FOUNDATION_EXPORT double CustomHttpHeadersURLProtocolVersionNumber;
+
+//! Project version string for CustomHttpHeadersURLProtocol.
+FOUNDATION_EXPORT const unsigned char CustomHttpHeadersURLProtocolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomHttpHeadersURLProtocol/PublicHeader.h>
+
+
